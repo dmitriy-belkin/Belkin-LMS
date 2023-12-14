85341dab490c9c69c64c552bccc3a6e3c6c46860
Dmitriy Belkin <demetrius.belkin@gmail.com>
2023-12-14 23:50:26 +0300

    navbar and fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './home/home.component.html',
-  styleUrl: './home/home.component.scss'
+  styleUrls: ['./home/home.component.scss']
 })
 export class AppComponent {
   title = 'Belkin LMS';
