6614d11a96088f0e7f1991d5bab1415ede25f641
Dmitriy Belkin <demetrius.belkin@gmail.com>
2023-12-15 21:47:41 +0300

    navbar well done =_O

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  dropdownOpen: boolean = false;
   public isMenuVisible: boolean = false;
 
   toggleMenu() {
     this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeDropdown() {
+    if (this.dropdownOpen) {
+      this.dropdownOpen = false;
+    }
   }
 }
